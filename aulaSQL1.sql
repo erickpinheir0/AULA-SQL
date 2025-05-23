b9f1a71b110e8a75e8b5938965563d068b7b51b0
Erick Pinheiro <114263898+erickpinheir0@users.noreply.github.com>
2025-05-23 11:57:25 -0300

    Add files via upload

--- aulaSQL1.sql
+++ aulaSQL1.sql
@@ -42,8 +42,113 @@ CREATE TABLE categoria (
 );
 
 CREATE TABLE produto (
-	idPedido INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	idProduto INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	nome VARCHAR(100) NOT NULL,
     preco DOUBLE DEFAULT 10,
     quantidade DOUBLE DEFAULT 0
 );
+
+
+
+CREATE TABLE eletronico (
+	codProduto INT NOT NULL PRIMARY KEY,
+    amperagem DOUBLE, 
+    voltagem DOUBLE,
+    bivolt BOOLEAN,
+    FOREIGN KEY (codProduto) REFERENCES produto(idProduto)
+);
+
+CREATE TABLE percivel (
+	codProduto INT NOT NULL PRIMARY KEY,
+    temperaturaMinima DOUBLE, 
+    temperaturaMaxima DOUBLE,
+    FOREIGN KEY (codProduto) REFERENCES produto(idProduto)
+);
+
+CREATE TABLE categoria_produto (
+	codCategoria INT NOT NULL,
+    codProduto INT NOT NULL,
+    PRIMARY KEY (codCategoria, codProduto), 
+    FOREIGN KEY (codCategoria) REFERENCES categoria(idCategoria),
+    FOREIGN KEY (codProduto) REFERENCES produto(idProduto)
+);
+
+CREATE TABLE pedido_produto (
+	codPedido INT NOT NULL,
+    codProduto INT NOT NULL,
+    precoVendido DOUBLE,
+    quantidadeVendida DOUBLE,
+    PRIMARY KEY (codPedido, codProduto), 
+    FOREIGN KEY (codPedido) REFERENCES pedido(idPedido),
+    FOREIGN KEY (codProduto) REFERENCES produto(idProduto)
+);
+
+
+DESCRIBE pessoa;
+
+INSERT INTO cidade (idCidade, nome) VALUES (1, "Porto Alegre");
+INSERT INTO cidade (nome, idCidade) VALUES ("Viamão", 2);
+INSERT INTO cidade  VALUES (3, "Canoas");
+INSERT INTO cidade  VALUES (NULL, "Alvorada");
+INSERT INTO cidade(nome) VALUES ("Capão da Canoa");
+
+INSERT INTO cidade(nome) VALUES
+("São Paulo"),
+("Florianópolis");
+
+SELECT * FROM cidade;
+
+INSERT INTO pessoa VALUES (NULL, "João", 1.85, "1985-05-20", "Rua A, 100", 3);
+
+INSERT INTO pessoa(nome, altura, codCidade) VALUES ("Maria", 1.78, 3);
+
+INSERT INTO pessoa(nome, altura, codCidade, nascimento, endereco) VALUES 
+("José", 1.78, 1, "1994-02-05", "Rua C, 200"),
+("Julia", 1.50, 4, "2005-10-18", "Rua D, 400");
+
+INSERT INTO pessoa(nome) VALUES ("Renata") , ("Roberto");
+
+SELECT * FROM pessoa;
+
+DELETE FROM cidade WHERE ( ( idCidade = 2 )  OR ( idCidade = 5 ) ); 
+
+
+
+INSERT INTO categoria ( nome ) VALUES 
+( "Bebidas" ) ,
+( "Alimentos" ) ,
+( "Limpeza" ) ;
+
+INSERT INTO produto (nome, preco, quantidade) VALUES
+( "Coca-cola" , 9.89 , 100  ) ,
+( "Pepsi" , 6.99 , 80  ) ,
+( "Trakinas" , 3.95 , 50  ) ,
+( "Arroz" , 3.99 , 100 ) ,
+( "Doritos" , 11.99 , 60 ) ,
+( "Fanta" , 7.99 , 180  );
+
+INSERT INTO categoria_produto VALUES 
+( 1 , 1 ) , 
+( 1 , 2 ) ,
+( 2 , 3 ) ,
+( 2 , 4 ) ,
+( 2 , 5 ) ,
+( 1 , 6 );
+
+INSERT INTO pedido ( horario , endereco , codCliente) VALUES
+( "2024-10-17 18:10:30" , "Rua A, 100" , 2 );
+
+INSERT INTO pedido ( endereco , codCliente) VALUES
+( "Rua B, 200" , 1 );
+
+INSERT INTO pedido (codCliente) VALUES ( 2 );
+
+INSERT INTO pedido_produto VALUES 
+( 1 , 1 ,  8.00 , 4 ) , 
+( 1 , 5 , 11.99 , 2 ) ,
+( 1 , 3 ,  3.95 , 1 ) ;
+
+INSERT INTO pedido_produto (codPedido, codProduto, precoVendido) VALUES 
+( 2 , 1 ,  9.89 ) , 
+( 2 , 4 ,  3.99 ) ;
+
